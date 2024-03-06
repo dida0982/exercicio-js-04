9c727cf620d76e6728d3c086359f7df480c5e6cd
Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>
2024-03-06 16:32:28 -0300

    Add files via upload

---
+++ exercicio-de-js-04/app.js
@@ -0,0 +1,8 @@
+// Solicita ao usuário que insira seu nome usando prompt e armazena o nome inserido
+let nomeUsuario = prompt("Por favor, insira seu nome:");
+
+// Cria uma mensagem de boas-vindas usando o nome inserido pelo usuário
+let mensagemBoasVindas = `Bem-vindo(a), ${nomeUsuario}!`;
+
+// Exibe um alerta de boas-vindas com a mensagem personalizada
+alert(mensagemBoasVindas);
